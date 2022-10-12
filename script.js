fe3249222d1c3492764fee7821b05cd4fb12bb30
j37hr0 <jethro.cotton3@gmail.com>
2022-10-12 14:38:44 +0200

    added loop for displaying boxes, touched up modals and layout

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //DOM queries
+const main = document.getElementById("main")
 const newbtn = document.getElementById("newBook");
 const modal = document.getElementById("modal")
 const closebtn = document.getElementsByClassName("close")[0]
@@ -9,7 +10,7 @@ const bookPages = document.getElementById("pages")
 const bookIsRead = document.getElementById("isRead")
 
 
-let myLibrary = [];
+let myLibrary = [{title: "test book1", author: "testauthor1", pages: 10, isRead: "yes"}];
 
 function createBook (title, author, nrPages, isRead) {
     this.title = title;
@@ -26,8 +27,25 @@ function createBook (title, author, nrPages, isRead) {
 //display books in myLibrary
 function displayBooks(library){
 for (let i = 0; i < library.length; i++){
+    let box = document.createElement("div");
     let removebtn = document.createElement("button");
     let togglebtn = document.createElement("button");
+    let p1 = document.createElement("p")
+    p1.innerHTML = library[i].title;
+    let p2 = document.createElement("p")
+    p2.innerHTML = library[i].author
+    let p3 = document.createElement("p")
+    p3.innerHTML = library[i].pages;
+    let p4 = document.createElement("p")
+    p4.innerHTML = library[i].isRead;
+    box.classList.add("box");
+    removebtn.classList.add("remove");
+    togglebtn.classList.add("toggle");
+    main.appendChild(box)
+    box.appendChild(p1)
+    box.appendChild(p2)
+    box.appendChild(p3)
+    box.appendChild(p4)
 }
 }
 
@@ -47,3 +65,4 @@ newbtn.onclick = function () {
     modal.style.display = "grid";
 }
 
+displayBooks(myLibrary)
