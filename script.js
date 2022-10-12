3580dfafa204cd4774b30d8a2789cd8ee5a20bc4
j37hr0 <jethro.cotton3@gmail.com>
2022-10-12 14:53:09 +0200

    got each book displaying on a card correctly

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ const bookPages = document.getElementById("pages")
 const bookIsRead = document.getElementById("isRead")
 
 
-let myLibrary = [{title: "test book1", author: "testauthor1", pages: 10, isRead: "yes"}];
+let myLibrary = [{title: "test book1", author: "testauthor1", pages: 10, isRead: "yes"},
+{title: "test book1", author: "testauthor1", pages: 10, isRead: "yes"},
+{title: "test book1", author: "testauthor1", pages: 10, isRead: "yes"},
+{title: "test book1", author: "testauthor1", pages: 10, isRead: "yes"},];
 
 function createBook (title, author, nrPages, isRead) {
     this.title = title;
@@ -30,14 +33,16 @@ for (let i = 0; i < library.length; i++){
     let box = document.createElement("div");
     let removebtn = document.createElement("button");
     let togglebtn = document.createElement("button");
+    removebtn.innerHTML = "Remove";
+    togglebtn.innerHTML = "Toggle Read"
     let p1 = document.createElement("p")
-    p1.innerHTML = library[i].title;
+    p1.innerHTML = `Title: ${library[i].title}`;
     let p2 = document.createElement("p")
-    p2.innerHTML = library[i].author
+    p2.innerHTML = `Author: ${library[i].author}`;
     let p3 = document.createElement("p")
-    p3.innerHTML = library[i].pages;
+    p3.innerHTML = `Pages: ${library[i].pages}`;
     let p4 = document.createElement("p")
-    p4.innerHTML = library[i].isRead;
+    p4.innerHTML = `Is Read: ${library[i].isRead}`;
     box.classList.add("box");
     removebtn.classList.add("remove");
     togglebtn.classList.add("toggle");
@@ -46,6 +51,10 @@ for (let i = 0; i < library.length; i++){
     box.appendChild(p2)
     box.appendChild(p3)
     box.appendChild(p4)
+    box.appendChild(togglebtn)
+    box.appendChild(removebtn)
+
+    
 }
 }
 
